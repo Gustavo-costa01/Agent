e5d5d69cdd2583bb2699c35b5fd3aded4aa96d0c
gustavo <gustavo.nascimeno196@etc.etec.sp.gov.br>
2025-06-10 21:16:17 -0300

    atualizacao16_03

--- Agent/Form1.Designer.cs
+++ Agent/Form1.Designer.cs
@@ -37,13 +37,13 @@
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Observação = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.obs_box = new System.Windows.Forms.TextBox();
             this.dtp_DataeHora = new System.Windows.Forms.DateTimePicker();
             this.btnRemove = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.Observação = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.obs_box = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -130,6 +130,11 @@
             this.columnHeader3.Text = "Date";
             this.columnHeader3.Width = 91;
             // 
+            // Observação
+            // 
+            this.Observação.Text = "Observação";
+            this.Observação.Width = 149;
+            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label4);
@@ -144,10 +149,27 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(27, 25);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(242, 304);
+            this.groupBox1.Size = new System.Drawing.Size(242, 245);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Agendamento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Observação";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // obs_box
+            // 
+            this.obs_box.Location = new System.Drawing.Point(80, 154);
+            this.obs_box.Name = "obs_box";
+            this.obs_box.Size = new System.Drawing.Size(140, 20);
+            this.obs_box.TabIndex = 8;
             // 
             // dtp_DataeHora
             // 
@@ -164,45 +186,23 @@
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(147, 275);
+            this.btnRemove.Location = new System.Drawing.Point(161, 213);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(75, 23);
             this.btnRemove.TabIndex = 6;
-            this.btnRemove.Text = "Remover";
+            this.btnRemove.Text = "Concluído";
             this.btnRemove.UseVisualStyleBackColor = true;
             this.btnRemove.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 275);
+            this.button1.Location = new System.Drawing.Point(78, 213);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(76, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "Agendar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Observação
-            // 
-            this.Observação.Text = "Observação";
-            this.Observação.Width = 149;
-            // 
-            // obs_box
-            // 
-            this.obs_box.Location = new System.Drawing.Point(80, 154);
-            this.obs_box.Name = "obs_box";
-            this.obs_box.Size = new System.Drawing.Size(140, 20);
-            this.obs_box.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 157);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Observação";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
